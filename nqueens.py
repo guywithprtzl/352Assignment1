2e00ced41e7cd8a554c1efbde6c56052dbc9923d
guywithprtzl <mynameisnotbobby@gmail.com>
2018-02-21 03:22:20 -0500

    initialize function

--- nqueens.py
+++ nqueens.py
@@ -1,7 +1,15 @@
 """
-Assignment 1
+CISC 352 Assignment 1
 nqueens.py
 Sean Tippett - 10108181
 Heni Virag - 10142490
 """
+import random
+
+def initialize(n):
+    initialMatrix = []
+    for i in range(1,n+1):
+        initialMatrix.append(i)
+    random.shuffle(initialMatrix)
+    return initialMatrix
 
