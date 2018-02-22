339a6c6662ff1acbb53ff97390e6295bb6d7346b
guywithprtzl <mynameisnotbobby@gmail.com>
2018-02-21 20:11:36 -0500

    leftDiag and rightDiag "done"

--- nqueens.py
+++ nqueens.py
@@ -9,6 +9,7 @@ Heni Virag - 10142490
 ############################
 """
 import random
+import sys
 
 def initialize(csp):
     initialMatrix = []
@@ -18,15 +19,25 @@ def initialize(csp):
     return initialMatrix
 
 
-def conflicts(queen, var, current, csp):
+def conflicts(queen, value, current, csp):
+    calculate
     pass
 
-def constraints(current):
+#checks
+def constraints(listOfConflicts):
+    
     pass
     
 
 
-
+def main():
+    solutions = []
+    with open("nqueens.txt", "r") as f:
+        csp = f.read()
+        solutions.append(min_conflicts(csp))
+    solutionsToString
+    with open(filename, "w") as f:
+        f.write('\n'.join(sizesAsString))
 
 """
 #################
@@ -38,17 +49,33 @@ Utility Functions
  takes in the current matrix,
     the index of the queen we care about 
 """
-def calculateLeftDiag(queen, current):
+def calculateLeftDiag(queen, current, csp):
     row = queen
     column = current[queen]
     conflicts = 0
-    #helpful code here
+    diagNum = row-column
+    otherRow = 0
+    while otherRow != (csp-1):
+        if otherRow != row:
+            otherColumn = current[otherRow]
+            otherDiagNum = otherRow - otherColumn
+            if diagNum == otherDiagNum:
+                conflicts++
+        otherRow++
     return conflicts
 
-def calculateRightDiag(queen, current):
+def calculateRightDiag(queen, current, csp):
     row = queen
     column = current[queen]
     conflicts = 0
-    #helpful code here
+    diagNum = row + column
+    otherRow = 0
+    while otherRow != (csp-1):
+        if otherRow != row:
+            otherColumn = current[otherRow]
+            otherDiagNum = otherRow + otherColumn
+            if diagNum == otherDiagNum:
+                conflicts++
+        otherRow++
     return conflicts
 
